353463f8dc93240fad37264a45b419b9f3dfdad5
deykson10 <deyksonaraujo@gmail.com>
2023-08-25 21:05:32 -0300

    gerador

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("TESTE")
+
+let sliderElement = document.querySelector("#slider");
+let buttonElent = document.querySelector("#button");
+
+let sizepassaword = document.querySelector("#valor");
+let passaword = document.querySelector("#passaword");
+
+let containerPassaword = document.querySelector("container-passaword");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXY0123456789!@";
+let novaSenha = "";
+
+sizepassaword.innerHTML = sliderElement.value;
+
+slider.oniput = function(){
+    sizepassaword.innerHTML = this.value;
+}
+
+function generatepassaword(){
+
+    let pass = "b!";
+    for()
+}
